7ebd9bf90ad797ccbbc44bc15bf6253bdc60807f
NOOR OMARI <nooromari847@gmail.com>
2021-01-27 15:44:28 +0200

    js file added

---
+++ app.js
@@ -0,0 +1,10 @@
+var user = prompt('what is your name?');
+alert('Hi '+ user );
+var userInput = prompt('How are you? good or not good');
+if (userInput=='good') { alert('Great');
+    
+} else if (userInput=='not good') {alert('ma3lesh')
+    
+} else{ alert('have a nice day')}
+confirm("Do you want to read more about reading skills?");
+
