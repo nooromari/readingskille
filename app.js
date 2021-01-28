bafad50a2aabf2575f9d3d1f529bfa4a857c2d8f
NOOR OMARI <nooromari847@gmail.com>
2021-01-28 15:54:12 +0200

    add dynamic image

--- app.js
+++ app.js
@@ -6,5 +6,25 @@ if (userInput=='good') { alert('Great');
 } else if (userInput=='not good') {alert('ma3lesh')
     
 } else{ alert('have a nice day')}
-confirm("Do you want to read more about reading skills?");
+var ans = prompt ('Do you want to read more about reading skills? yes or no' );
 
+while (!(ans=='yes'|| ans =='no')) {
+     ans = prompt ('please write yes or no only!' );
+}
+
+if (ans == 'yes') { alert('you can press in (Back to the main page) in the end of the page')
+    
+}
+
+var num = prompt ('How many books do you read?' );
+var image='' ;
+
+if (num == 0) {
+    image = '<img src ="https://ctp-media.imigino.com/image/1/process/nullxnull?source=https://northernnatalcourier.co.za/wp-content/uploads/sites/63/2016/10/7qfcq5.jpg">';
+} else  {
+
+for (i = 0; i < num; i++) {
+    image =image + '<img src ="https://image.shutterstock.com/image-vector/happy-smiley-emoticon-reading-book-260nw-228260320.jpg">';
+}
+}
+document.write(image);
